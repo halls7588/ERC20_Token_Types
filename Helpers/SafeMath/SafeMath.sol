84a1b62fabde3bdb52b69c7732d070c20ea76f5b
Stephen Hall <halls7588@gmail.com>
2018-05-21 11:19:18 -0700

    Add files via upload

---
+++ Helpers/SafeMath/SafeMath.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.23;
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ * @notice https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/contracts/math/SafeMath.sol
+ */
+library SafeMath {
+	/**
+	 * SafeMath mul function
+	 * @dev function for safe multiply
+	 **/
+	function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+		uint256 c = a * b;
+		assert(a == 0 || c / a == b);
+		return c;
+	}
+
+	/**
+	 * SafeMath div funciotn
+	 * @dev function for safe devide
+	 **/
+	function div(uint256 a, uint256 b) internal pure returns (uint256) {
+		uint256 c = a / b;
+		return c;
+	}
+
+	/**
+	 * SafeMath sub function
+	 * @dev function for safe subtraction
+	 **/
+	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+		assert(b <= a);
+		return a - b;
+	}
+}
