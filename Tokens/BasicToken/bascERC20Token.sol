bb49f389fc229e4ae26382ccdae9c571c44d4398
Stephen Hall <halls7588@gmail.com>
2018-05-21 12:06:41 -0700

    Update and rename bascToken.sol to bascERC20Token.sol

---
+++ Tokens/BasicToken/bascERC20Token.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.23;
+
+import "../../Helpers/TokenBasics/StandardToken.sol";
+
+/**
+ * @title Lavevel Token
+ * @dev Simple ERC20 Token with standard token functions.
+ */
+contract LavevelToken is StandardToken {
+  string public constant NAME = "Lavevel Token";
+  string public constant SYMBOL = "LVL";
+  uint256 public constant DECIMALS = 18;
+
+  uint256 public constant INITIAL_SUPPLY = 500000000 * 10**18;
+
+  /**
+   * Lavevel Token Constructor
+   * @dev Create and issue tokens to msg.sender.
+   */
+  constructor() public {
+    totalSupply = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+  }
+}
