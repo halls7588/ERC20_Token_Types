51ec98394f4e54302544e5aeaa15504ce2b3f155
Stephen Hall <halls7588@gmail.com>
2018-05-21 14:30:42 -0700

    Update mintableToken.sol

--- Tokens/MintableToken/mintableToken.sol
+++ Tokens/MintableToken/mintableToken.sol
@@ -1,1 +1,66 @@
 pragma solidity ^0.4.23;
+
+import "../../Helpers/TokenBasics/StandardToken.sol";
+import "../../Helpers/Modifiers/Ownable.sol";
+
+/**
+ * @title Mintable token
+ * @dev Simple ERC20 Token example, with mintable token creation
+ * Based on code by TokenMarketNet: https://github.com/TokenMarketNet/ico/blob/master/contracts/MintableToken.sol
+ */
+contract MintableToken is StandardToken, Ownable {
+  string public constant NAME = "MintableToken Token";
+  string public constant SYMBOL = "MNT";
+  uint256 public constant DECIMALS = 18;
+  uint256 public constant INITIAL_SUPPLY = 500000000 * 10**18;
+  
+  event Mint(address indexed to, uint256 amount);
+  event MintFinished();
+
+  bool public mintingFinished = false;
+
+
+  modifier canMint() {
+    require(!mintingFinished);
+    _;
+  }
+
+  modifier hasMintPermission() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Function to mint tokens
+   * @param _to The address that will receive the minted tokens.
+   * @param _amount The amount of tokens to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
+  function mint(address _to, uint256 _amount) hasMintPermission canMint public returns (bool) {
+    totalSupply_ = totalSupply_.add(_amount);
+    balances[_to] = balances[_to].add(_amount);
+    emit Mint(_to, _amount);
+    emit Transfer(address(0), _to, _amount);
+    return true;
+  }
+
+  /**
+   * @dev Function to stop minting new tokens.
+   * @return True if the operation was successful.
+   */
+  function finishMinting() onlyOwner canMint public returns (bool) {
+    mintingFinished = true;
+    emit MintFinished();
+    return true;
+  }
+  
+  
+  /**
+   * MintableToken Token Constructor
+   * @dev Create and issue tokens to msg.sender.
+   */
+  constructor() public {
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+  } 
+}
